8d33a59e3baa188036e344b9b72776a96573e2ec
nilsgeiselhart <nigeiselh@gmail.com>
2017-10-22 12:13:53 +0200

    build script

---
+++ build_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "--- building docker image... ---"
+sudo docker build --tag nilsg/webinterface .
+
+echo "--- running docker container... ---"
+sudo docker run -it --rm -p 80:3000 nilsg/webinterface ./entrypoint.sh
